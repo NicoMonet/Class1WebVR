b5d7b61e902a6ce02e3927bcc6ff650f00101fa7
Nicolas Monet <mone0082@algonquinlive.com>
2019-01-11 14:25:14 -0500

    Adding server code

---
+++ app.js
@@ -0,0 +1,20 @@
+const express   = require('express');
+const app       = express();
+const http      = require('http');
+const server    = http.createServer(app);
+
+//convenience vars
+const LISTEN_PORT = 8080;
+
+//middleware This set out base html to emerge from the publicm folder
+app.use(express.static(___dirname + '/pubplic'));
+
+//set a route
+app.get('/', function(req, res){
+    res.sendFile(___dirname + 'public/index.html');
+});
+
+//finally, start server
+server.listen(LISTEN_PORT);
+
+console.log("Listening on port: " + LISTEN_PORT); //print statement to console/terminal
